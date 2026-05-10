8141d52b893201c5b58bbea4a3fcbfa4c19e2db5
unknown <abhivaddoriya2017@gamil.com>
2026-05-10 14:49:27 +0530

    Update CORS logic and allowed origins for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,8 +1,4 @@
-import app from '../backend/app.js';
-import { connectDB } from '../backend/config/db.js';
-
-// Connect to the database on cold start
-connectDB();
+import app from './app.js';
 
 // Export the Express app as a module for Vercel Serverless Function
 export default app;
